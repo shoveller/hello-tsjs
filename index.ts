db4c471402d3ca68c3d4aea210585d3c4372627a
shoveller <cinos81@gmail.com>
2021-01-06 09:01:37 +0900

    나의 모델 저장하기 구현중

--- index.ts
+++ index.ts
@@ -1,57 +1,78 @@
-import * as tf from '@tensorflow/tfjs'
-import { SymbolicTensor } from "@tensorflow/tfjs-layers/src/engine/topology";
+import * as tf from '@tensorflow/tfjs-node'
+import path from 'path'
 
-/**
- * 1. 가르칠 데이터를 마련한다
- */
-const 온도 = [20,21,22,23]
-const 판매량 = [40,42,44,46]
-const 원인 = tf.tensor(온도)
-const 결과 = tf.tensor(판매량)
+(async () => {
+  /**
+   * 1. 가르칠 데이터를 마련한다
+   */
+  const 온도 = [20,21,22,23]
+  const 판매량 = [40,42,44,46]
+  const 원인 = tf.tensor(온도)
+  const 결과 = tf.tensor(판매량)
 
-/**
- * 2. 모델의 모양을 만든다
- */
-const inputs = tf.input({
-  // 입력이 1개라는 뜻
-  shape: [1]
-})
-const outputs  = tf.layers.dense({
-  // 출력이 1개라는 뜻
-  units: 1
-}).apply(inputs) as tf.SymbolicTensor[]
-const model = tf.model({ inputs, outputs })
-const compileParam: tf.ModelCompileArgs = {
-  optimizer: tf.train.adam(),
-  loss: tf.losses.meanSquaredError
-}
-model.compile(compileParam)
+  /**
+   * 2. 모델의 모양을 만든다
+   */
+  const inputs = tf.input({
+    // 입력이 1개라는 뜻
+    shape: [1]
+  })
+  const outputs  = tf.layers.dense({
+    // 출력이 1개라는 뜻
+    units: 1
+  }).apply(inputs) as tf.SymbolicTensor[]
+  const model = tf.model({ inputs, outputs })
+  const compileParam: tf.ModelCompileArgs = {
+    optimizer: tf.train.adam(),
+    // 평균 제곱 오차를 손실함수로 사용한다
+    loss: tf.losses.meanSquaredError
+  }
+  model.compile(compileParam)
 
-/**
- * 3. 데이터로 모델을 학습시킨다
- */
+  /**
+   * 3. 데이터로 모델을 학습시킨다
+   */
 // 학습 횟수는 100번
-const fitParam: tf.ModelFitArgs = { epochs: 100 }
-// const fitParam: tf.ModelFitArgs = {
-//   epochs: 100,
-//   callbacks: {
-//     onEpochEnd(epoch, logs) {
-//       console.log(epoch, logs);
-//     }
-//   }
-// }
-model.fit(원인, 결과, fitParam).then(result => {
-  // 4. 모델을 이용합니다.
+// const fitParam: tf.ModelFitArgs = { epochs: 100 }
+  const fitParam: tf.ModelFitArgs = {
+    // 학습 횟수는 10000번
+    epochs: 10000,
+    callbacks: {
+      onEpochEnd(epoch, logs = {}) {
+        // 평균 제곱 오차에 루트를 씌우면 평균 재곱근 오차가 된다.
+        // 평균 제곱근 오차가 0에 가까워질수록 학습이 잘 된 것이다.
+        console.log(epoch, logs, 'RMSE => ', Math.sqrt(logs.loss));
+      }
+    }
+  }
+  await model.fit(원인, 결과, fitParam)
+
+  // 4. 모델을 이용한다.
   // 4.1 기존의 데이터를 이용
   const predicts = model.predict(원인);
 
+  // 5. 모델의 실체는 입력값 * 가중치 + bias로 나타나는 1차함수
+  // @ts-ignore
+  const weight = model.getWeights()[0].arraySync()[0][0]
+  // @ts-ignore
+  const bias = model.getWeights()[1].arraySync()[0]
+  console.log('가중치 =>', weight);
+  console.log('bias =>', bias);
   console.log(predicts.toString());
-})
 
-/**
- * 4. 새로운 데이터를 이용한다
- */
-const 다음주온도 = [15, 16, 17, 18, 19]
-const 다음주원인 = tf.tensor(다음주온도)
-const 다음주결과 = model.predict(다음주원인)
-다음주원인.print()
+// 6. 모델을 저장한다
+  await model.save(`file://${path.join(process.cwd(), 'hello-tsjs-model')}`).then(r => {
+    console.log(r);
+  });
+
+  console.log('종료');
+
+// /**
+//  * 4. 새로운 데이터를 이용한다
+//  */k
+// const 다음주온도 = [15, 16, 17, 18, 19]
+// const 다음주원인 = tf.tensor(다음주온도)
+// const 다음주결과 = model.predict(다음주원인)
+// 다음주원인.print()
+})()
+
